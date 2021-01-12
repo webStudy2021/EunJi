a0f834a3ee28656bba2c8f8fb3fa3450312aac8f
qkrdmswl <eunji303@naver.com>
2021-01-12 17:38:12 +0900

    js_arrowBtn

--- main.js
+++ main.js
@@ -16,12 +16,30 @@ document.addEventListener('scroll', ()=>{
 //Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
-    const target= event.target;
+    const target = event.target;
     const link = target.dataset.link;
     if (link == null ) {
         return;
     }
-    console.log(event.target.dataset.link);
-    const scrollTo = document.querySelector(link);
+    console.log(link);
+    scrollIntoView(link);
+});
+
+// Handle click on "contact me" button on home
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', () => {
+    scrollIntoView('#contact');
+});
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: 'smooth'});
+}
+
+
+//Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', ()=>{
+    home.style.opacity = 1 - window.scrollY / homeHeight;
 });
